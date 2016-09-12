14d42dfcbbc96b6fd8176518bde651d272d5bfad
rainsilence0911 <rainsilence@qq.com>
2016-09-12 15:28:34 +0800

    add post css support

--- reactES6/webpack.config.js
+++ reactES6/webpack.config.js
@@ -3,43 +3,47 @@ var webpack = require('webpack');
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var values = require('postcss-modules-values');
 
 module.exports = {
-	entry : [ 
-		"webpack/hot/dev-server",	// // [webpack-dev-server config] hot module refresh require
-		"./app/main.js"
-	],
-	output : {
-		path : BUILD_PATH,
-		publicPath: "/assets/",		// [webpack-dev-server config]
-		filename : "[name].js"
-	},
-	module : {
-		loaders : [ {
-			test : /\.js$/,
-			exclude : /node_modules/,
-			loader : 'babel-loader'
-		}, {
-			test : /\.html$/,
-			loader : "html-loader"
-		}, {
-			test : /\.css$/,
-			loader : "style!css"
-		}, {
-			test : /\.less/,
-			loader : 'style-loader!css-loader!less-loader'
-		}, {
-			test : [ /\.png$/, /\.jpg$/, /\.jpeg$/, /\.gif$/, /\.svg$/, /\.ogg$/ ],
-			loader : "file-loader"
-		}]
-	},
-	"devtool" : "eval",
-	"debug" : true,
-	resolve : {
-		extensions : [ '', '.js', '.json' ]
-	},
-	plugins : [
-		new webpack.NoErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin()	// // [webpack-dev-server config] hot module refresh require
-	]
+    entry : [
+        "webpack/hot/dev-server",    // // [webpack-dev-server config] hot module refresh require
+        "./app/main.js"
+    ],
+    output : {
+        path : BUILD_PATH,
+        publicPath: "/assets/",        // [webpack-dev-server config]
+        filename : "[name].js"
+    },
+    module : {
+        loaders : [ {
+            test : /\.js$/,
+            exclude : /node_modules/,
+            loader : 'babel-loader'
+        }, {
+            test : /\.html$/,
+            loader : "html-loader"
+        }, {
+            test : /\.css$/,
+            loader : "style-loader!css-loader?modules!postcss-loader"
+        }, {
+            test : /\.less/,
+            loader : 'style-loader!css-loader!less-loader'
+        }, {
+            test : [ /\.png$/, /\.jpg$/, /\.jpeg$/, /\.gif$/, /\.svg$/ ],
+            loader : "file-loader"
+        }]
+    },
+    "devtool" : "eval",
+    "debug" : true,
+    postcss: [
+        values
+      ],
+    resolve : {
+        extensions : [ '', '.js', '.json' ]
+    },
+    plugins : [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin()    // // [webpack-dev-server config] hot module refresh require
+    ]
 };

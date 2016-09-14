384a728cad92e5c27596b06f0f993a3ec2cd8b61
Rainsilence <rainsilence@qq.com>
2016-09-14 16:49:15 +0800

    add recording support

--- reactES6/app/js/component/ChordLayer.js
+++ reactES6/app/js/component/ChordLayer.js
@@ -1,14 +1,17 @@
+
 import React from 'react';
 
 import GraphHelper from '../util/GraphHelper';
 import PositionUtil from '../util/PositionUtil';
 import Timer from '../util/Timer';
-
-import configs from '../config/ChordConfig';
+import ESUtil from '../util/ESUtil';
 
 import Chord from '../part/Chord';
+
 import EventManager from '../event/EventManager';
 import Events from '../event/Events';
+
+import configs from '../config/ChordConfig';
 
 import style from '../../css/style.css';
 
@@ -21,6 +24,10 @@ export default class ChordLayer extends React.Component {
         };
         this.lastY = null;
         this.chords = [];
+        this.keyMapper = {};
+        this.soundList = [];
+        this.isReplaying = false;
+        this.isRecording = false;
     }
 
     componentDidMount() {
@@ -39,14 +46,26 @@ export default class ChordLayer extends React.Component {
 
         }).bind(this));
 
+        timer.onComplete = this.onFrameComplete.bind(this);
+
         for (let i = 0; i < configs.length; i++) {
-            var chord = new Chord(graphHelper, configs[i], timer);
+            var config = configs[i];
+            var chord = new Chord(graphHelper, config, timer);
             chord.draw();
             this.chords.push(chord);
+            this.keyMapper[config.keyCode] = i;
         }
+
+        EventManager.register(Events.KEY_DOWN, this.handleKeyDown.bind(this));
+        EventManager.register(Events.RECORD_AUDIO, this.handleRecordAudio.bind(this));
+        EventManager.register(Events.REPLAY_AUDIO, this.handleReplayAudio.bind(this));
     }
 
     handleMouseMove(e) {
+
+        if (this.isReplaying) {
+            return;
+        }
 
         let event = e.nativeEvent;
 
@@ -67,14 +86,92 @@ export default class ChordLayer extends React.Component {
             let chord = chords[i];
 
             if (chord.hitTest(this.lastY, mouseX, mouseY)) {
-                chord.play(this.lastY > mouseY, mouseX);
-                EventManager.fire(Events.PLAY_AUDIO, {
-                    id: chord.id
-                });
+                this.playAudio(i, this.lastY > mouseY, mouseX);
             }
         }
 
         this.lastY = mouseY;
+    }
+
+    handleKeyDown(e) {
+
+        if (this.isReplaying) {
+            return;
+        }
+
+        let index = this.keyMapper[e.keyCode];
+
+        if (index == null) {
+            return;
+        }
+
+        this.playAudio(index, false);
+    }
+
+    handleRecordAudio() {
+        this.isRecording = true;
+        this.soundList = [];
+    }
+
+    handleReplayAudio(e) {
+
+        this.isRecording = false;
+        this.isReplaying = true;
+        let soundIter = ESUtil.getIterator(this.soundList);
+
+        let current = soundIter.next();
+
+        if (current.done) {
+            return;
+        }
+
+        this.replayAudio(soundIter, current.value);
+    }
+
+    onFrameComplete() {
+
+        // if last sound and timer complete, it will trigger isReplaying = false
+        if (this.isReplaying === false ||
+            this.soundList.length !== 0) {
+            return;
+        }
+
+        this.isReplaying = false;
+        EventManager.fire(Events.REPLAY_FINISHED);
+    }
+
+    replayAudio(soundIter, currentSound) {
+
+        this.playAudio(currentSound.index, currentSound.isReverse, currentSound.mouseX);
+
+        let next = soundIter.next();
+
+        if (next.done) {
+            this.soundList = [];
+            return;
+        }
+
+        let nextSound = next.value;
+        let elapse = nextSound.time - currentSound.time;
+        setTimeout((function() {
+            this.replayAudio(soundIter, nextSound);
+        }).bind(this), elapse);
+    }
+
+    playAudio(index, isReverse, mouseX) {
+        let chord = this.chords[index];
+        chord.play(isReverse, mouseX);
+        EventManager.fire(Events.PLAY_AUDIO, {
+            id: chord.id
+        });
+        if (this.isRecording) {
+            this.soundList.push({
+                index: index,
+                isReverse: isReverse,
+                time: new Date().getTime(),
+                mouseX: mouseX
+            });
+        }
     }
 
     render() {

9dbc3d328decef9320fcb7ad1557881b0400e0f1
Rainsilence <rainsilence@qq.com>
2016-09-14 16:51:01 +0800

    add recording support

--- reactES6/app/js/event/Events.js
+++ reactES6/app/js/event/Events.js
@@ -1,3 +1,7 @@
 export default {
-    PLAY_AUDIO: "play_audio"
+    PLAY_AUDIO: "play_audio",
+    RECORD_AUDIO: "record_audio",
+    REPLAY_AUDIO: "replay_audio",
+    REPLAY_FINISHED: "replay_finished",
+    KEY_DOWN: "key_down"
 }

6280862e1b1643f079dd6dc7dae76934f233ad5b
Rainsilence <rainsilence@qq.com>
2016-09-14 16:48:45 +0800

    add recording support

--- reactES6/app/js/component/Background.js
+++ reactES6/app/js/component/Background.js
@@ -1,6 +1,7 @@
-import style from '../../css/style.css';
 
 import React from 'react';
+
+import style from '../../css/style.css';
 
 export default class Background extends React.Component {
     constructor() {
